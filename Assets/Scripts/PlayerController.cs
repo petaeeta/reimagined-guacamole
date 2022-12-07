58478b41cbe6ec1e39877383d5b8dc6b799f4ace
Petteri Taro <ext-petteri.taro@casambi.com>
2022-12-07 20:39:34 +0200

    first code snippets

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5.0f;
+    public float turnSpeed;
+    public float horizontalInput;
+    public float verticalInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        // Moves the vehicle forward
+        transform.Translate(verticalInput * speed * Time.deltaTime * Vector3.forward);
+        transform.Rotate(Vector3.up * horizontalInput * Time.deltaTime * turnSpeed);
+    }
+}
